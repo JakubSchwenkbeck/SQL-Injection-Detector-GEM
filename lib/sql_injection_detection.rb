65e7ed87d027156ca225ecd72b93164aba24d5dd
Jakub Schwenkbeck <Jakub@eSchwenkbeck>
2024-08-26 23:37:38 +0200

    init commit

--- lib/sql_injection_detection.rb
+++ lib/sql_injection_detection.rb
@@ -1,8 +1,17 @@
-# frozen_string_literal: true
-
-require_relative "sql_injection_detection/version"
-
 module SqlInjectionDetection
-  class Error < StandardError; end
-  # Your code goes here...
+  class Checker
+    # Common patterns for SQL Injection
+    PATTERNS = [
+      /(\%27)|(\')|(\-\-)|(\%23)|(#)/ix, # SQL meta-characters
+      /\b(OR|AND)\b/i,                   # Boolean operators
+      /\b(UNION)\b/i,                    # UNION keyword
+      /\b(SELECT)\b/i                    # SELECT keyword
+    ]
+
+    def self.check(input)
+      return false if input.nil? || input.strip.empty?
+
+      PATTERNS.any? { |pattern| pattern.match?(input) }
+    end
+  end
 end
--- spec/sql_injection_detection_spec.rb
+++ spec/sql_injection_detection_spec.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-
+require 'sql_injection_detection'
 RSpec.describe SqlInjectionDetection do
   it "has a version number" do
     expect(SqlInjectionDetection::VERSION).not_to be nil
