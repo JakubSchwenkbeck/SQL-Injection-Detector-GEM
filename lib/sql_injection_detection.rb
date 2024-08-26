57d25697711a678aa5b06106bcf6c0a59addb1a8
Jakub Schwenkbeck <Jakub@eSchwenkbeck>
2024-08-26 23:37:23 +0200

    init

---
+++ lib/sql_injection_detection.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "sql_injection_detection/version"
+
+module SqlInjectionDetection
+  class Error < StandardError; end
+  # Your code goes here...
+end
